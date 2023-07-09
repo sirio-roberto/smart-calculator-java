c510ab6b63020e11565b2fcf829adfe565b7a0ea
Sirio <sirioroberto1@hotmail.com>
2023-07-08 22:48:33 -0300

    Implemented methos to sanitize userInput and convert minus meaning. In addition, now we can add and subtract with our calculator

--- calculator/Main.java
+++ calculator/Main.java
@@ -1,7 +1,8 @@
 package calculator;
 
-import java.util.Arrays;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
@@ -17,17 +18,45 @@ public class Main {
         do {
             userInput = SCAN.nextLine();
             if ("/help".equals(userInput)) {
-                System.out.println("The program calculates the sum of numbers");
+                System.out.println("The program calculates the sum and subtraction of numbers");
             }
             else if (!userInput.isBlank() && !"/exit".equals(userInput)) {
+                userInput = removeExtraSpacesAndPlus(userInput);
+                userInput = convertMinus(userInput);
                 String[] userInputFields = userInput.split(" ");
-                if (Arrays.stream(userInputFields).allMatch(p -> p.matches("-?\\d+"))) {
-                    int sum = Arrays.stream(userInputFields).map(Integer::parseInt).reduce(0, Integer::sum);
-                    System.out.println(sum);
-                } else {
-                    System.out.println("Only numbers are supported");
+
+                int total = Integer.parseInt(userInputFields[0]);
+                for (int i = 1; i < userInputFields.length; i += 2) {
+                    int nextNumber = Integer.parseInt(userInputFields[i + 1]);
+                    switch (userInputFields[i]) {
+                        case "+" -> total += nextNumber;
+                        case "-" -> total -= nextNumber;
+                    }
                 }
+                System.out.println(total);
             }
         } while (!"/exit".equals(userInput));
+    }
+
+    private static String convertMinus(String userInput) {
+        Pattern minusPattern = Pattern.compile("-{2,}");
+        String[] expressionArray = userInput.split(" ");
+        for (int i = 0; i < expressionArray.length; i++) {
+            Matcher multipleMinusMatcher = minusPattern.matcher(expressionArray[i]);
+            if (multipleMinusMatcher.matches()) {
+                // if the amount of minus is even, we replace it with plus, otherwise we replace with one minus
+                expressionArray[i] = expressionArray[i].length() % 2 == 0 ? "+" : "-";
+            }
+        }
+        return String.join(" ", expressionArray);
+    }
+
+    private static String removeExtraSpacesAndPlus(String userInput) {
+        Matcher matcherSpaces = Pattern.compile("\\s{2,}").matcher(userInput);
+        String result = matcherSpaces.replaceAll(" ");
+
+        Matcher matcherPlus = Pattern.compile("\\+{2,}").matcher(result);
+        result = matcherPlus.replaceAll("+");
+        return result;
     }
 }
