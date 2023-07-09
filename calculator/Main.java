9ec9eb501c544b85586a9e6cae9d9b1ac8e7fcdb
Sirio <sirioroberto1@hotmail.com>
2023-07-09 19:59:34 -0300

    Optimized imports and changed the /help command to reflect the whole program logic

--- calculator/Main.java
+++ calculator/Main.java
@@ -1,6 +1,5 @@
 package calculator;
 
-import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.*;
 import java.util.regex.Matcher;
@@ -212,8 +211,35 @@ public class Main {
         if (userInput.matches("/+.*") && !Arrays.asList(VALID_COMMANDS).contains(userInput)) {
             System.out.println("Unknown command");
         } else if ("/help".equals(userInput)) {
-            // TODO: adjust helper text before last commit
-            System.out.println("The program calculates the sum and subtraction of numbers");
+            System.out.println("""
+                    The program is a simple calculator that allows you to perform arithmetic calculations and store variables. It supports the following features:
+                                        
+                        Arithmetic Operations:
+                            Addition (+)
+                            Subtraction (-)
+                            Multiplication (*)
+                            Division (/)
+                            Exponentiation (^)
+                                        
+                        Variable Assignments:
+                            Assign values to variables using the "=" operator.
+                            Variables must start with a letter and can contain letters and numbers.
+                            Example: x = 5
+                                        
+                        Usage:
+                            Enter expressions to perform calculations or assignments.
+                            Examples: 2 + 3, y = x + 5, x * (y - 2)
+                                        
+                        Command Support:
+                            Use the following commands to interact with the program:
+                                "/help" - Display this help message.
+                                "/exit" - Exit the program.
+                                        
+                        Notes:
+                            Spaces are allowed between numbers, operators, and parentheses.
+                            Expressions can be entered on a single line or multiple lines.
+                            Parentheses can be used to group operations and control precedence.
+                            The program supports both integer and decimal arithmetic with high precision.""");
         } else {
             System.out.println("Bye!");
         }
